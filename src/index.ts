d23dbf1a2e71e983353a991b17423e4485bdf359
nshiab <nael.shiab@gmail.com>
2026-03-10 13:58:30 -0400

    Removing helper functions

--- src/index.ts
+++ src/index.ts
@@ -27,13 +27,9 @@ import getYahooFinanceData from "./finance/getYahooFinanceData.ts";
 import variableMortgagePayments from "./finance/variableMortgagePayments.ts";
 import simulateRentVsBuy from "./finance/simulateRentVsBuy.ts";
 import simulateRentVsBuyMonteCarlo from "./finance/simulateRentVsBuyMonteCarlo.ts";
-import getParamsRentVsBuy from "../test/finance/helpers/getParamsRentVsBuy.ts";
-import getParamsRentVsBuyMonteCarlo from "../test/finance/helpers/getParamsRentVsBuyMonteCarlot.ts";
 
 export {
   adjustToInflation,
-  getParamsRentVsBuy,
-  getParamsRentVsBuyMonteCarlo,
   getYahooFinanceData,
   mortgageInsurancePremium,
   mortgageMaxAmount,
